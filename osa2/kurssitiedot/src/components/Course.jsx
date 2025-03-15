cf24cbbca174fe1a992b7f93cc5cf669bdf52ca9
rautiais <rautiainen.e.salla@gmail.com>
2025-03-15 13:16:56 +0200

    Task 2.5

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -8,45 +8,44 @@ const Course = ({ course }) => {
   )
 }
 
-  const Header = (props) => {
-    console.log(props)
-    return(
+const Header = (props) => {
+  console.log(props)
+  return(
+  <div>
+    <h2> {props.course} </h2>
+  </div>
+)
+}
+
+const Content = (props) => {
+  return(
     <div>
-      <h2> {props.course} </h2>
+      {props.parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
     </div>
   )
-  }
+}
 
-  const Content = (props) => {
-    return(
-      <div>
-        {props.parts.map(part => (
-          <Part key={part.id} part={part} />
-        ))}
-      </div>
-    )
-  }
+const Part = (props) => {
+  return(
+    <div>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </div>
+  )
+}
 
-    const Part = (props) => {
-      return(
-        <div>
-          <p>
-            {props.part.name} {props.part.exercises}
-          </p>
-        </div>
-      )
-    }
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return(
+    <div>
+      <b>
+        total of {total} exercises
+      </b>
+    </div>
+  )
+}
 
-  
-  const Total = (props) => {
-    const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-    return(
-      <div>
-        <b>
-          total of {total} exercises
-        </b>
-      </div>
-    )
-  }
-
-  export default Course
+export default Course
