72ad48261f671fbd061b0e0c7dc02fe5319dd79d
rautiais <rautiainen.e.salla@gmail.com>
2026-03-20 15:49:17 +0200

    Teht. 3.2

--- osa3/index.js
+++ osa3/index.js
@@ -28,6 +28,14 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/info', (request, response) => {
+  const date = new Date()
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
