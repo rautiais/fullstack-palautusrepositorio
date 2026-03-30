6a914b34f3bca0d0bdc9d1c6812302c13e0a919f
rautiais <rautiainen.e.salla@gmail.com>
2026-03-30 13:44:40 +0300

    Teht. 3.15-3.18

--- osa3/index.js
+++ osa3/index.js
@@ -4,6 +4,16 @@ const morgan = require("morgan");
 const app = express();
 const Person = require("./models/person");
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+}
+
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use((_req, res, next) => {
@@ -34,19 +44,37 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end();
-  });
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(error => next(error))
+});
+
+app.put("/api/persons/:id", (request, response) => {
+  const { number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -69,6 +97,8 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson);
   });
 });
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
   app.listen(PORT, () => {
