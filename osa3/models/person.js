ff737fdf5ead02e46d753d15bd981ba2b2dbcb19
rautiais <rautiainen.e.salla@gmail.com>
2026-04-03 18:41:44 +0300

    Teht. 3.20

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -14,13 +14,28 @@ mongoose.connect(url, { family: 4 })
     console.log('error connecting to MongoDB:', error.message)
   })
 
+const validatePhoneNumber = function(v) {
+  const parts = v.split('-')
+  if (parts.length !== 2) return false
+  if (parts[0].length < 2 || parts[0].length > 3) return false
+  return /^\d+$/.test(parts[0]) && /^\d+$/.test(parts[1])
+}
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: validatePhoneNumber,
+      message: (props) => `${props.value} is not a valid phone number! Use format XX-XXXXXXX or XXX-XXXXXXXX`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
