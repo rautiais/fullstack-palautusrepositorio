4dd773e98e50219182abdbcf4c05c30a01a420a4
rautiais <rautiainen.e.salla@gmail.com>
2026-04-03 17:56:57 +0300

    Teht. 3.21

--- osa2/puhelinluettelo/src/main.jsx
+++ osa2/puhelinluettelo/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
 import App from "./App";
-const baseUrl = "/api/notes";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
