6cd3e0578cbdde259d78b4a35a053e06fd438df5
rautiais <rautiainen.e.salla@gmail.com>
2026-04-04 17:49:39 +0300

    Teht. 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
