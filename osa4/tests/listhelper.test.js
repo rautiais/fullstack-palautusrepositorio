4b82c5cdf9b3fa39bbef3c7e2f75109d32a7a8fb
rautiais <rautiainen.e.salla@gmail.com>
2026-04-09 16:41:56 +0300

    Changed the file name to make it more clear

--- osa4/tests/listhelper.test.js
+++ osa4/tests/listhelper.test.js
@@ -1,6 +1,6 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const listHelper = require('../utils/for_testing')
+const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
   const blogs = []
