478e89c9d163e92264469dc07e51e1646abb7501
rautiais <rautiainen.e.salla@gmail.com>
2026-04-09 16:27:43 +0300

    Teht. 4.21

--- osa4/app.js
+++ osa4/app.js
@@ -54,7 +54,21 @@ app.put("/api/blogs/:id", async (request, response, next) => {
 
 app.delete("/api/blogs/:id", async (request, response, next) => {
   try {
-    await Blog.findByIdAndDelete(request.params.id);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+      return response.status(404).json({ error: 'blog not found' })
+    }
+
+    if (blog.user.toString() !== decodedToken.id.toString()) {
+      return response.status(403).json({ error: 'only the creator can delete this blog' })
+    }
+
+    await blog.deleteOne()
     response.status(204).end();
   } catch (error) {
     next(error);
